b5e78fc4a0d254fc10ba5411f43729e2e1f182ed
Tudor1703 <tudormirca90@gmail.com>
2025-01-21 20:27:51 +0200

    the problem is solved!

--- sales_site/shop_app/admin.py
+++ sales_site/shop_app/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Car
 
-@admin.register(Car)
-class CarAdmin(admin.ModelAdmin):
-    list_display = ('make', 'model', 'year', 'mileage', 'fuel_type', 'price', 'description', 'type', 'image')  # Columns in admin listing
-    search_fields = ('make', 'model', 'year')  # Search fields
+admin.site.register(Car)
