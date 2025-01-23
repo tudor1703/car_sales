fa62b78dbc217b0d45447a6405de6eda060c5265
Tudor1703 <tudormirca90@gmail.com>
2025-01-23 17:15:27 +0200

    improving style

--- sales_site/shop_app/views.py
+++ sales_site/shop_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Car
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def car_list(request):
     shop_app = Car.objects.all()
-    return render(request, 'shop_app/shopApp.html', {'shop_app': shop_app})
+
+    p = Paginator(Car.objects.all(), 2)
+    page = request.GET.get('page')
+    cars = p.get_page(page)
+
+    return render(request, 'shop_app/shopApp.html', 
+                  {'shop_app': shop_app,
+                   'cars': cars})
