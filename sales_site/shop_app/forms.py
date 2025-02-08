d74d3156eb45c8df8370721da43a537597844526
Tudor1703 <tudormirca90@gmail.com>
2025-02-08 16:38:31 +0200

    fixed errors with ruff

--- sales_site/shop_app/forms.py
+++ sales_site/shop_app/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 
 CHOICES = [
-    ('make', 'Make'),
-    ('model', 'Model'),
-    ('year', 'Year'),
+    ("make", "Make"),
+    ("model", "Model"),
+    ("year", "Year"),
 ]
+
 
 class OrderForm(forms.Form):
     order_by = forms.ChoiceField(
--- sales_site/shop_app/urls.py
+++ sales_site/shop_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cars/', views.car_list, name='car_list'),
+    path("cars/", views.car_list, name="car_list"),
 ]
