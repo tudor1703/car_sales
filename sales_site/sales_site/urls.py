5fd46aec027cac6f880d6111e976431a77e9920c
Tudor1703 <tudormirca90@gmail.com>
2025-02-13 21:23:10 +0200

    Configured ruff

--- sales_site/sales_site/urls.py
+++ sales_site/sales_site/urls.py
@@ -3,6 +3,7 @@ URL configuration for sales_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +16,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
