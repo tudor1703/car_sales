428607b01434fee07ab646850c3838a893f4b110
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2025-03-09 13:20:14 +0200

    Revert "Sixth step"

--- sales_site/shop_app/models.py
+++ sales_site/shop_app/models.py
@@ -1,30 +1,32 @@
 from django.db import models
-
+from django.contrib import admin
 
 class Car(models.Model):
     TRANSMISION_TYPE_CHOICE = [
-        ("mecanica", "Mecanica"),
-        ("automata", "Automata"),
+        ('mecanica', 'Mecanica'),
+        ('automata', 'Automata'),
     ]
     CONSUME_TYPE = [
-        ("diesel", "Diesel"),
-        ("plugin_hybrid", "Plug-in Hybrid"),
-        ("hybrid", "Hybrid"),
-        ("benzine", "Benzine"),
-        ("electric", "Electric"),
+        ('diesel', 'Diesel'),
+        ('plugin_hybrid', 'Plug-in Hybrid'),
+        ('hybrid', 'Hybrid'),
+        ('benzine', 'Benzine'),
+        ('electric', 'Electric'),
     ]
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.PositiveBigIntegerField()
     mileage = models.PositiveBigIntegerField(default=0)
-    fuel_type = models.CharField(max_length=16, choices=CONSUME_TYPE, default="diesel")
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(null=True, blank=True)
-    type = models.CharField(
-        max_length=10, choices=TRANSMISION_TYPE_CHOICE, default="mecanica"
+    fuel_type = models.CharField(
+        max_length=16, choices=CONSUME_TYPE, default='diesel'
     )
-    image = models.ImageField(upload_to="car_images/", null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    type = models.CharField(
+        max_length=10, choices=TRANSMISION_TYPE_CHOICE, default='mecanica'
+    )
+    image = models.ImageField(upload_to='car_images/', null=True, blank=True)
     order = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.year} {self.make} {self.model}"
+        return f'{self.year} {self.make} {self.model}'
--- sales_site/shop_app/static/shop_app/js/alert.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".btn-close").forEach(button => {
-        button.addEventListener("click", function () {
-            this.parentElement.classList.add("hide"); // Hides the alert
-            setTimeout(() => this.parentElement.remove(), 300); // Removes from DOM
-        });
-    });
-});
